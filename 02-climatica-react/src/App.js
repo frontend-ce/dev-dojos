2669dd31441bbb1fd8cea871de811453444a0d07
Riva Junior <jrcavalcantejr@gmail.com>
2019-07-04 16:58:10 -0300

    Fix npm securities audits

--- 02-climatica-react/src/App.js
+++ 02-climatica-react/src/App.js
@@ -1,60 +1,66 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.scss'
-import ClimaLocal from './components/ClimaLocal';
-import BuscaCidades from './components/BuscarCidades';
+import "./App.scss";
+import ClimaLocal from "./components/ClimaLocal";
+import BuscaCidades from "./components/BuscarCidades";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            location: {
-                lat: null,
-                lon: null
-            },
-        };
-    }
+    this.state = {
+      location: {
+        lat: null,
+        lon: null
+      }
+    };
+  }
 
-    componentWillMount() {
-        navigator.geolocation.getCurrentPosition(this.updateLocation);
-    }
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition(this.updateLocation);
+  }
 
-    updateLocation = (position) => {
-        const { latitude, longitude } = position.coords;
-        const location = {
-            lat: latitude,
-            lon: longitude
-        };
-        this.getClimaAtualPorPosicao(latitude, longitude)
-        this.setState({ ...this.state, location });
-    }
+  updateLocation = position => {
+    const { latitude, longitude } = position.coords;
+    const location = {
+      lat: latitude,
+      lon: longitude
+    };
+    this.getClimaAtualPorPosicao(latitude, longitude);
+    this.setState({ ...this.state, location });
+  };
 
-    getClimaAtualPorPosicao = (lat, lon) => {
-        axios
-            .get(`http://api.openweathermap.org/data/2.5/weather?APPID=e26ac13ac93e35d3f18a0fe899791d93&lat=${lat}&lon=${lon}&units=metric&lang=pt`)
-            .then(response => {
-                this.setState({ ...this.state, climaAtual: response.data });
-            });
-    }
+  getClimaAtualPorPosicao = (lat, lon) => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?APPID=e26ac13ac93e35d3f18a0fe899791d93&lat=${lat}&lon=${lon}&units=metric&lang=pt`
+      )
+      .then(response => {
+        this.setState({ ...this.state, climaAtual: response.data });
+      });
+  };
 
-    getClimaAtualPorCidade = (cidade) => {
-        this.setState({ ...this.state, climaAtual: null })
+  getClimaAtualPorCidade = cidade => {
+    this.setState({ ...this.state, climaAtual: null });
 
-        axios
-            .get(`http://api.openweathermap.org/data/2.5/weather?APPID=e26ac13ac93e35d3f18a0fe899791d93&lang=pt&units=metric&q=${encodeURI(cidade)},BR`)
-            .then(response => {
-                this.setState({ ...this.state, climaAtual: response.data });
-            });
-    }
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?APPID=e26ac13ac93e35d3f18a0fe899791d93&lang=pt&units=metric&q=${encodeURI(
+          cidade
+        )},BR`
+      )
+      .then(response => {
+        this.setState({ ...this.state, climaAtual: response.data });
+      });
+  };
 
-    render() {
-        return (
-            <div>
-                <ClimaLocal climaAtual={this.state.climaAtual} />
-                <BuscaCidades getClimaAtualPorCidade={this.getClimaAtualPorCidade} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <ClimaLocal climaAtual={this.state.climaAtual} />
+        <BuscaCidades getClimaAtualPorCidade={this.getClimaAtualPorCidade} />
+      </div>
+    );
+  }
 }
